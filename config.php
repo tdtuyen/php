069ad35ec95a76f2ade58362ea248a516690dd31
TuyenTran-C1220G1 <77034359+TuyenTran-C1220G1@users.noreply.github.com>
2021-07-17 23:06:24 +0700

    config db

--- config.php
+++ config.php
@@ -1,5 +1,6 @@
 <?php
-define('HOST', 'localhost');
-define('DATABASE', 'student');
-define('USERNAME', 'root');
-define('PASSWORD', '');
+define('HOST', 'us-cdbr-east-04.cleardb.com');
+define('DATABASE', 'heroku_89fc12fa4f000f8');
+define('USERNAME', 'bd65a02c631e11');
+define('PASSWORD', 'e9a604bf');
+//mysql://bd65a02c631e11:e9a604bf@us-cdbr-east-04.cleardb.com/heroku_89fc12fa4f000f8?reconnect=true
